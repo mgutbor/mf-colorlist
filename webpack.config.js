436283e2244d9cfe0b0d61e15c9e782113c928f4
mgutbor <mgutbor@gmail.com>
2024-04-03 18:55:53 +0200

    dev mode en webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,66 +2,68 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const Dotenv = require('dotenv-webpack');
 const deps = require("./package.json").dependencies;
-module.exports = (_, argv) => ({
-  output: {
-    publicPath: "https://mmxlab-colorlist-mfcolorpicker.netlify.app/",
-  },
+module.exports = (_, argv) => {
+  return {
+    output: {
+      publicPath: argv.mode === "development" ? "http://localhost:3002/" : "https://mmxlab-colorlist-mfcolorpicker.netlify.app/",
+    },
 
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
-  },
+    resolve: {
+      extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+    },
 
-  devServer: {
-    port: 3002,
-    historyApiFallback: true,
-  },
+    devServer: {
+      port: 3002, 
+      historyApiFallback: true,
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.m?js/,
-        type: "javascript/auto",
-        resolve: {
-          fullySpecified: false,
+    module: {
+      rules: [
+        {
+          test: /\.m?js/,
+          type: "javascript/auto",
+          resolve: {
+            fullySpecified: false,
+          },
         },
-      },
-      {
-        test: /\.(css|s[ac]ss)$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
+        {
+          test: /\.(css|s[ac]ss)$/i,
+          use: ["style-loader", "css-loader", "postcss-loader"],
         },
-      },
+        {
+          test: /\.(ts|tsx|js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+      ],
+    },
+
+    plugins: [
+      new ModuleFederationPlugin({
+        name: "colorlist",
+        filename: "remoteEntry.js",
+        remotes: {},
+        exposes: {
+          "./ColorList": "./src/components/ColorList.jsx",
+        },
+        shared: {
+          ...deps,
+          react: {
+            singleton: true,
+            requiredVersion: deps.react,
+          },
+          "react-dom": {
+            singleton: true,
+            requiredVersion: deps["react-dom"],
+          },
+        },
+      }),
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+      }),
+      new Dotenv()
     ],
-  },
-
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "colorlist",
-      filename: "remoteEntry.js",
-      remotes: {},
-      exposes: {
-        "./ColorList": "./src/components/ColorList.jsx",
-      },
-      shared: {
-        ...deps,
-        react: {
-          singleton: true,
-          requiredVersion: deps.react,
-        },
-        "react-dom": {
-          singleton: true,
-          requiredVersion: deps["react-dom"],
-        },
-      },
-    }),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-    }),
-    new Dotenv()
-  ],
-});
+  }
+};
